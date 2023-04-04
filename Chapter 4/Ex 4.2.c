fca80452405ced232af798a4535c3b7088b40b91
Itunu Atilade Shitta <108689630+Atilade01@users.noreply.github.com>
2023-04-04 09:48:05 +0100

    Answer to question 4.2 added
    QUESTION: Extend atof to handle scientific notation of the form 123.45e-6 
    where a floating-point number may be followed by e or E and an optionally signed exponent.

--- Chapter 4/Ex 4.2.c
+++ Chapter 4/Ex 4.2.c
@@ -1,4 +1,4 @@
-/* QUESTION: Extend atof to handle scientific notation of the form 123.45e-6 
+/* QUESTION 4.2: Extend atof to handle scientific notation of the form 123.45e-6 
 where a floating-point number may be followed by e or E and an optionally signed exponent. */
 
 #include <stdio.h>
