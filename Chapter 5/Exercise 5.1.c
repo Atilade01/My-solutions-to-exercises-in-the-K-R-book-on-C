542e319be3c06b2944c02851c27d18fd1b17c2f2
Itunu Atilade Shitta <108689630+Atilade01@users.noreply.github.com>
2024-07-26 15:55:42 +0100

    Update Exercise 5.1.c
    Two versions are included here

--- Chapter 5/Exercise 5.1.c
+++ Chapter 5/Exercise 5.1.c
@@ -1,3 +1,10 @@
+/*
+I have written the code below in two forms. The first uses the same functions (getch() and ungetch() )in the K & R  book.
+These functions are provided as part of the conio.h library. They are not provided in stdio.h.
+The second uses the stdio functions getchar() and ungetc(). Note that we could also use getc(FILE *) instead of getchar() if we prefer.
+*/
+
+/* VERSION 1 */
 #include <stdio.h>
 #include <ctype.h>
 
@@ -5,10 +12,70 @@ int getint(int *);
 int main() 
 {
     // Write C code here
-    int a = 0;
+    int a;
     printf("The value is: %d ",getint(&a));
-    printf("\nFinally,the value of a is set to: %d ",a);
+    return 0;
+}
 
+#define BUFSIZE 100
+char buf[BUFSIZE];    // buffer for ungetch
+int bufp = 0;         // next free position in buf
+int getch(void)  // get a (possibly pushed-back) character
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+void ungetch(int c)   // push character back on input
+{
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
+}
+
+/* getint:  get next integer from input into *pn */
+int getint(int *pn)
+{
+    int c, sign;
+    while (isspace(c = getch()))   /* skip white space */
+        ;
+    if (!isdigit(c) && c != EOF && c != '+' && c != '-')
+    {
+        ungetch(c);  /* it is not a number */
+        return 0;
+    }
+    sign = (c == '-') ? -1 : 1;
+    if (c == '+' || c == '-')
+    {
+        c = getch();
+        if(!isdigit(c))
+        {
+            ungetch(c);
+            return 0;
+        }
+    }
+    for (*pn = 0;isdigit(c); c = getch())
+    {
+        *pn = 10 * *pn + (c - '0');
+    }
+    *pn *= sign;
+    if (c != EOF)
+    {
+        ungetch(c);
+    }
+    return c;
+}
+
+
+
+/* VERSION 2 */
+#include <stdio.h>
+#include <ctype.h>
+
+int getint(int *);
+int main() 
+{
+    // Write C code here
+    int a;
     return 0;
 }
 
@@ -18,7 +85,6 @@ int getint(int *pn)
     int c, sign;
     while (isspace(c = getchar()))   /* skip white space */
         ;
-    printf("Begin, c is: %d\n",c);
     if (!isdigit(c) && c != EOF && c != '+' && c != '-')
     {
         ungetc(c,stdin);  /* it is not a number */
@@ -28,28 +94,21 @@ int getint(int *pn)
     if (c == '+' || c == '-')
     {
         c = getchar();
+        // the above could also be written as c = getc(stdin)
         if(!isdigit(c))
         {
-            printf("In if() func, c is: %d\n",c);
             ungetc(c,stdin);
             return 0;
         }
     }
     for (*pn = 0;isdigit(c); c = getchar())
     {
-        printf("In for() loop, c is: %d\n",c);
         *pn = 10 * *pn + (c - '0');
-        printf("In for() loop, pn is: %d\n",*pn);
     }
     *pn *= sign;
     if (c != EOF)
     {
-        printf("We have not reached EOF\n");
         ungetc(c,stdin);
-    }
-    else
-    {
-        printf("We have reached EOF\n");
     }
     return c;
 }
